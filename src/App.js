1c2b9a55b63bcac648b595e9945b13c49d9bc9b5
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-08-18 22:51:51 +0200

    back and front connected, useeffect and, axios and map added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/comics`);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // const test = data.results;
+  // console.log(test);
+  // test.map((element) => {
+
+  //   return element.title;
+  // });
+
+  return isLoading ? (
+    <span>Nous feuilletons les oeuvres</span>
+  ) : (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hey
+      {data.results.map((element, index) => {
+        return <span key={index}>{element.title}</span>;
+      })}
     </div>
   );
 }
