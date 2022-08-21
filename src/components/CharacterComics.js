55fe61f8eedfbef78102d15f9965698b4f6fc356
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-08-21 21:53:57 +0200

    comics carroussel working

--- src/components/CharacterComics.js
+++ src/components/CharacterComics.js
@@ -1,12 +1,40 @@
-const CharacterComics = ({ comicsInfo }) => {
-  console.log(comicsInfo);
+// import { useState } from "react";
+
+const CharacterComics = ({
+  comicsInfo,
+  setTitle,
+  setDescription,
+  setHighLight,
+  highLight,
+  key2,
+}) => {
+  //   const [highlight, setHighLight] = useState(``);
+
+  //   const newTab = [...highLight];
+  //   newTab.push(false);
+  //   setHighLight(newTab);
 
   return (
-    <div>
-      <div>
-        <h3>{comicsInfo.title}</h3>
-        <span></span>
-      </div>
+    <div
+      onClick={() => {
+        setTitle(comicsInfo.title); //onClick to collecte the comics Data
+        setDescription(comicsInfo.description);
+        const newTab = [...highLight];
+        for (let i = 0; i < newTab.length; i++) {
+          if (i === key2) {
+            newTab[i] = true;
+          } else {
+            newTab[i] = false;
+          }
+        }
+
+        // newTab[key2] = true;
+        console.log(newTab);
+        setHighLight(newTab);
+      }}
+      className={highLight[key2] ? `test` : ``}
+      onBlur
+    >
       <img
         src={`${comicsInfo.thumbnail.path}.${comicsInfo.thumbnail.extension}`}
         alt=""
--- src/pages/Character.js
+++ src/pages/Character.js
@@ -7,6 +7,13 @@ const Character = () => {
   const { id } = useParams();
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [title, setTitle] = useState(``); //Dynamic title when user choose a comics
+  const [description, setDescription] = useState(``); //Dynamic description when user choose a comics
+
+  const [highLight, setHighLight] = useState();
+
+  //   const highlighting = () => {
+  //
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +23,18 @@ const Character = () => {
         );
         setData(response.data);
         setIsLoading(false);
+
+        const newTab = [];
+
+        const test = (par) => {
+          for (let i = 0; i < par; i++) {
+            newTab.push(false);
+          }
+        };
+
+        test(response.data.comics.length);
+        console.log(newTab);
+        setHighLight(newTab);
       } catch (error) {
         console.log(error.response);
       }
@@ -39,8 +58,30 @@ const Character = () => {
       </div>
       <div className="characterComics">
         {data.comics.map((element, index) => {
-          return <CharacterComics key={index} comicsInfo={element} />;
+          return (
+            <CharacterComics
+              key={index}
+              key2={index}
+              comicsInfo={element}
+              setTitle={setTitle} //send to the component, states will changes with an Onclick on the component
+              setDescription={setDescription} // onClick cannot be setup here
+              className={highLight[index] && `test`}
+              setHighLight={setHighLight}
+              highLight={highLight}
+            />
+          );
         })}
+        {/* {data.comics.map((element) => {
+          return highlighting;
+        })} */}
+      </div>
+      <div className="comicsHiglight">
+        <div>
+          <h3>{title ? title : data.comics[0].title}</h3>{" "}
+          {/*the  default informations are from the first comics books of the database*/}
+          <p>{description ? description : data.comics[0].description}</p>
+        </div>
+        <img src="" alt="" />
       </div>
     </div>
   );
