4755a3640a9bfa79156cd505b0282f82668b57e7
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-08-26 01:01:29 +0200

    favorites page set-up

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const Favorites = ({ favorites }) => {
-  console.log(favorites);
+  //   console.log(favorites);
 
-  return <div className="container">{`test`}</div>;
+  return (
+    <div className="container">
+      <div>
+        <h2>Personnages</h2>
+        <div>
+          {favorites.characters.map((element, index) => {
+            return (
+              <div key={index}>
+                {console.log(element.name)}
+                <h3>{element.name}</h3>
+                <img
+                  src={`${element.thumbnail.path}.${element.thumbnail.extension}`}
+                  alt=""
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <h2> Comics</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Favorites;
